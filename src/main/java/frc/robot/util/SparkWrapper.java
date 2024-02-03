2ffb240a7778226b040151e4ab3025d398b921d3
Cody Belcher <cody.t.belcher@gmail.com>
2024-02-02 18:20:37 -0600

    .

--- src/main/java/frc/robot/util/SparkWrapper.java
+++ src/main/java/frc/robot/util/SparkWrapper.java
@@ -10,8 +10,9 @@ public class SparkWrapper extends CANSparkMax {
 
     @Override
     public void setInverted(boolean isInverted) {
-        super.setInverted(isInverted);
+        
         for (int i = 0; i <= 5; i++) {
+            super.setInverted(isInverted);
             if (super.getInverted() == isInverted) {
                 break;
             }
